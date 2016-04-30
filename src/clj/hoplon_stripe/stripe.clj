6972d47e28f626833dd79e33c99be410b398440e
leontalbot <leontalbot@gmail.com>
2016-04-30 16:17:28 -0400

    remove tests

--- src/clj/hoplon_stripe/stripe.clj
+++ src/clj/hoplon_stripe/stripe.clj
@@ -3,7 +3,6 @@
             [ring.util.codec :refer [form-encode]]))
 
 
-
 (def sk-token "sk_test_kkkkkkkkkkkkkkkkkkkkkkkkk")
 
 (defn create-charge [body]
@@ -14,13 +13,6 @@
                 :accept :json
                 :throw-exceptions false
                 }))
-
-#_(sh/use-token! (:stripe-secret env/config))
-
-#_(sh/use-token! "sk_test_Evoth804PfKzuCF2B1BMNfK5")
-
-#_(defn create-charge [options]
-  (sh/post-req "charges" {:stripe-params options}))
 
 (defn handle-charge [params]
   (let [p params]
@@ -33,20 +25,3 @@
         :description (str "Order from " (-> p :form :email) ": ")
         :metadata    (-> p :form)})
       (-> p :form))))
-
-;; TESTS
-#_(create-charge {:source "tok_15p25FCKaFkL2HYPSiWprPzc"
-                  :amount "2000"
-                  :currency "cad"
-                  :description "repl test"
-                  :metadata {:name "HI!"}})
-
-#_(create-charge "amount=2000&currency=cad&source=tok_185hdLCKaFkL2HYPoR2I1tHi&description=postman_test")
-
-
-;; RPC
-#_(->> params
-     (stripe/handle-charge (-> data/db :bazar :prices))
-     stripe/handle-response-after-charge
-     (fb/conj-and-return! fb/bazar-inscriptions)
-     (views/bazar-step-3 (-> data/db :bazar :prices)))
